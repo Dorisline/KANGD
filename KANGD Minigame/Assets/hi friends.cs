f68e1f30462ca8446c4be7c8c378063ff62cbade
Dorisline <jayromanisc@gmail.com>
2021-09-19 18:41:53 -0400

    test
    test

---
+++ KANGD Minigame/Assets/hi friends.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hifriends : MonoBehaviour
+{
+    // I made this for testing purposes
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
